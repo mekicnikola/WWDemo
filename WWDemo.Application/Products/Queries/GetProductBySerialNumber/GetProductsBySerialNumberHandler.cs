16b04240000456369e98669768992c27fc2af071
nikolamekic <nikolamekic@gmail.com>
2024-02-26 14:52:57 +0100

    Added implementation of GetProductsBySerialNumberHandler.cs

--- WWDemo.Application/Products/Queries/GetProductBySerialNumber/GetProductsBySerialNumberHandler.cs
+++ WWDemo.Application/Products/Queries/GetProductBySerialNumber/GetProductsBySerialNumberHandler.cs
@@ -1,13 +1,28 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using WWDemo.Application.DTOs;
+using WWDemo.Data.Products;
 
 namespace WWDemo.Application.Products.Queries.GetProductBySerialNumber
 {
     public class GetProductsBySerialNumberHandler : IRequestHandler<GetProductBySerialNumberQuery, ProductRepresentation>
     {
-        public Task<ProductRepresentation> Handle(GetProductBySerialNumberQuery request, CancellationToken cancellationToken)
+        private readonly IProductRepository _productRepository;
+        private readonly IMapper _mapper;
+
+        public GetProductsBySerialNumberHandler(IProductRepository productRepository, IMapper mapper)
         {
-            throw new NotImplementedException();
+            _productRepository = productRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ProductRepresentation> Handle(GetProductBySerialNumberQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetProductBySerialNumber("");
+
+            var result = _mapper.Map<Models.Product, DTOs.ProductRepresentation>(product!);
+
+            return result;
         }
     }
 }
